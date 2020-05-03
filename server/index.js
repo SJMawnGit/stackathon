0460ce402a1c98fa8325801e42f30fc07273986e
SJMawn <SJmawn@gmail.com>
2020-05-02 21:04:03 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const volleyball = require('volleyball')
+
+const app = express()
+
+app.use(volleyball)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.use('/api', require('./api'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  })
+
+
+module.exports = app
